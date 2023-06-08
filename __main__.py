8d5d9ce896fe8bad7fd941f06de1d3c58ec14acc
RPF511 <sbk98726@gmail.com>
2023-06-08 14:11:27 +0900

    base

--- __main__.py
+++ __main__.py
@@ -1,9 +1,11 @@
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, request,Response, jsonify, send_file
+from flask_cors import CORS, cross_origin
 from Modules import Visualizer
 import json
 
 
 app = Flask(__name__, template_folder="templates")
+CORS(app, resources={r'*': {'origins': '*'}})
 
 vis = Visualizer("20221118_cid_freq_srs.csv")
 
@@ -16,10 +18,11 @@ def params():
     return send_file(f'Data/temp.jpg', mimetype='image/gif')
 
 
-@app.route('/predict')
+@app.route('/predict',methods=['GET','POST'])
 def predict():
     data = request.json
-    return str('0')
+    print(data)
+    return Response(json.dumps({"result" : 12}))
 
 
 if __name__ == "__main__":
